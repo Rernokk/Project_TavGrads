1bb860d68be5f47e79203ac05cb6818442460379
UFP\ssmith37 <ssmith37@uccs.edu>
2019-08-17 22:53:31 -0600

    Very quick setup for easily building and integrating new menu elements for future minigames.

---
+++ MiniGradTavern/Assets/Resources/Tavern_Hub/MinigameDataList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GAME_ENUM
+{
+	MINI_GAME,
+}
+
+public struct MinigameData
+{
+	public string TavernPrefabName;
+
+	public MinigameData(string TavernPrefabName)
+	{
+		this.TavernPrefabName = TavernPrefabName;
+	}
+}
+
+public static class MinigameDataList
+{
+	public static Dictionary<GAME_ENUM, MinigameData> ActiveMinigames = new Dictionary<GAME_ENUM, MinigameData>()
+	{
+		{GAME_ENUM.MINI_GAME, new MinigameData("MinigameBottle")},
+	};
+}
