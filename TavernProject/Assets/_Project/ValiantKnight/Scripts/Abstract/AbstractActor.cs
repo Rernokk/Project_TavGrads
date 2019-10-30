ef64545022e2369afea137067ee7524b5d510459
Kevin Moore <zyronn.sorrow@gmail.com>
2019-10-29 21:16:00 -0600

    Added PlayerActor

--- TavernProject/Assets/_Project/ValiantKnight/Scripts/Abstract/AbstractActor.cs
+++ TavernProject/Assets/_Project/ValiantKnight/Scripts/Abstract/AbstractActor.cs
@@ -6,18 +6,22 @@ public abstract class AbstractActor : MonoBehaviour
 	[Header("Health and Combat Stats")]
 	[SerializeField]
 	[Tooltip("The maximum amount of health that the actor has (float).")]
-	private float maxHealth;
+	protected float maxHealth;
 
 	[SerializeField]
 	[Tooltip("The amount of damage the actor deals when attacking (float).")]
-	private float attackDamage;
+	protected float attackDamage;
 	#endregion
 
 	#region Variables
-	private float health;
+	protected float health;
 	#endregion
 
 	#region Methods
+	/// <summary>
+	/// Updates the actor's health value by the passed amount.
+	/// </summary>
+	/// <param name="amount">The amount to change the health value by. Positive will increase health, negative will decrease health.</param>
 	protected abstract void UpdateHealth(int amount);
 
 	protected abstract void Attack();
---
+++ TavernProject/Assets/_Project/ValiantKnight/Scripts/Actors/PlayerActor.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : AbstractActor
+{
+	#region Serialize Fields
+	[Header("Movement")]
+	[SerializeField]
+	[Tooltip("How quickly the player moves (float).")]
+	private float moveSpeed;
+
+	[SerializeField]
+	[Tooltip("The maximum run speed of the player (float).")]
+	private float maxMoveSpeed;
+
+	[SerializeField]
+	[Tooltip("How much the player's run speed increases when the associated timer expires (float).")]
+	private float moveSpeedIncrementAmount;
+
+	[SerializeField]
+	[Tooltip("How quickly the player slides (float).")]
+	private float slideSpeed;
+
+	[SerializeField]
+	[Tooltip("How high the player jumps (float).")]
+	private float jumpHeight;
+
+	[Header("Coroutines")]
+	[SerializeField]
+	[Tooltip("The delay in seconds before the increase run speed timer goes off (float).")]
+	private float moveSpeedIncreaseTimerDelay;
+	#endregion
+
+	#region Methods
+	protected override void UpdateHealth(int amount)
+	{
+		health += amount;
+
+		if(health <= 0f)
+		{
+			// TODO player death logic
+			Debug.Log(this.gameObject.name + " is DEAD");
+		}
+
+		// prevent the player from gaining more than their max health value
+		if(health >= maxHealth)
+		{
+			health = maxHealth;
+		}
+	}
+
+	protected override void Attack()
+	{
+		// TODO attacking logic
+		Debug.Log(this.gameObject.name + " ATTACKS");
+	}
+
+	private void Move()
+	{
+		// TODO player movement logic
+	}
+
+	private void Jump()
+	{
+		// TODO player jumping logic
+	}
+
+	private void Slide()
+	{
+		// TODO player sliding logic
+	}
+
+	private void IncreaseSpeed()
+	{
+		// TODO increase player run speed up to a maximum value
+	}
+	#endregion
+
+	#region MonoBehaviour
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+
+	protected override void Update()
+	{
+		// TODO player update logic
+	}
+
+	protected override void OnTriggerEnter2D(Collider2D col)
+	{
+		// TODO player trigger collision logic
+	}
+	#endregion
+
+	#region Coroutines
+	private IEnumerator NextSpeedIncrease(float delay)
+	{
+		// TODO player speed increase on timer
+		yield return new WaitForSeconds(delay);
+	}
+	#endregion
+}
